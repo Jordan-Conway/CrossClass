c8ef1f1e3102c9a9046191bbf73142e43acdb7a2
Jordan Conway <jordan_conway@outlook.com>
2026-02-08 19:19:28 +0000

    Fix double free and add unit tests

---
+++ tests/line_data_list_tests/line_data_list_tests.h
@@ -0,0 +1,6 @@
+#ifndef LINE_DATA_LISTS_TESTS
+#define LINE_DATA_LISTS_TESTS
+
+int run_line_data_list_tests();
+
+#endif
---
+++ tests/test.c
@@ -0,0 +1,7 @@
+#include "./line_data_list_tests/line_data_list_tests.h"
+
+int main() {
+    run_line_data_list_tests();
+
+    return 0;
+}
