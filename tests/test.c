79360a046e5c87b7adb08f216b6e5260dbf85f4a
Jordan Conway <jordan_conway@outlook.com>
2026-02-08 21:42:14 +0000

    Move test suite init to test.c

--- tests/test.c
+++ tests/test.c
@@ -1,7 +1,28 @@
 #include "./line_data_list_tests/line_data_list_tests.h"
+#include <CUnit/Basic.h>
+#include <CUnit/TestDB.h>
 
 int main() {
-    run_line_data_list_tests();
+    CU_pSuite test_suite = NULL;
+
+    if (CU_initialize_registry() != CUE_SUCCESS){
+        return CU_get_error();
+    }
+
+    test_suite = CU_add_suite("CrossClass Test Suite", NULL, NULL);
+
+    if (test_suite == NULL) {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    add_line_data_list_tests(test_suite);
+
+    CU_basic_set_mode(CU_BRM_VERBOSE);
+    CU_basic_run_tests();
+    CU_cleanup_registry();
+
+    return CU_get_error();
 
     return 0;
 }
