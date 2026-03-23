6579f2729af1b4af52ac90b8ede0ba8a4c941747
Jordan Conway <jordan_conway@outlook.com>
2026-03-23 12:40:37 +0000

    Move dep install

--- .ci/test.sh
+++ .ci/test.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+sudo apt-get install libcunit1 libcunit1-doc libcunit1-dev
+
+if [ $? -ne 0 ]; then
+    echo "Failed to install dependencies"
+    exit 1
+fi
+
+echo "Dependencies installed"
+
 ./build/tests/bin/crossclass_tests
 
 if [ $? -ne 0 ]; then
